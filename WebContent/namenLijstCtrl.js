cea052d342fb28cb5e63b91076d5ab770421e0ec
Unknown <rcwvandenberg@gmail.com>
2017-01-20 14:43:21 +0100

    Namenlijst versimpeld voor testdoeleinden

--- WebContent/namenLijstCtrl.js
+++ WebContent/namenLijstCtrl.js
@@ -1,8 +1,11 @@
 angular.module('familie')
 .controller('namenLijstCtrl', function($scope, $http) {
+	$scope.achternaam = "van den Berg"
+/*
 	$http.get('http://localhost:8080/Familie/rest/person/all').
     then(function(response) {
     	alert(response.data)
         $scope.persons = response.data;
     });
+*/
 });
