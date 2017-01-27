c5100aba92a406b8f3ce241f1cb5116ef07f91e3
Unknown <rcwvandenberg@gmail.com>
2017-01-27 09:04:12 +0100

    Namenlijst 0.2

--- WebContent/app.js
+++ WebContent/app.js
@@ -11,9 +11,17 @@ app.config(function($routeProvider) {
         templateUrl : "namenlijst.html",
         controller  : "namenlijstCtrl"
     })
+    .when("/persoon/:id", {
+        templateUrl : "persoon.html",
+        controller  : "persoonCtrl"
+    })
 ;
 });
 app.controller("namenlijstCtrl", function ($scope, $http) {
 	$http.get("http://localhost:8080/Familie/rest/person/all")
 	.then(function(response) {$scope.persons = response.data});
+});
+app.controller("persoonCtrl", function ($scope, $http, $routeParams) {
+	$http.get("http://localhost:8080/Familie/rest/person/one/" + $routeParams.id)
+	.then(function(response) {$scope.person = response.data});
 });
